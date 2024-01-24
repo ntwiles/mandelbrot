9e9307a90d1b084ea0722b91158610a8d3688656
Nathan Wiles <ntwiles@gmail.com>
2024-01-23 23:32:31 -0500

    Calculate a and b outside of mandlebrot struct.

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,20 @@ mod mandelbrot;
 
 use mandelbrot::Mandelbrot;
 
-const WIDTH: u32 = 300;
-const HEIGHT: u32 = 300;
+const VIEWPORT_WIDTH: u32 = 300;
+const VIEWPORT_HEIGHT: u32 = 300;
+const ZOOM: f64 = 1.0;
+
 const DIVERGE_THRESHOLD: f64 = 16.0;
 const DIVERGE_ITERATIONS: u32 = 100;
-const ZOOM: f64 = 1.0;
 
 fn main() -> Result<(), Error> {
     env_logger::init();
     let event_loop = EventLoop::new();
 
     let window = {
-        let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
-        let scaled_size = LogicalSize::new(WIDTH as f64 * ZOOM, HEIGHT as f64 * ZOOM);
+        let size = LogicalSize::new(VIEWPORT_WIDTH as f64, VIEWPORT_HEIGHT as f64);
+        let scaled_size = LogicalSize::new(VIEWPORT_WIDTH as f64 * ZOOM, VIEWPORT_HEIGHT as f64 * ZOOM);
 
         WindowBuilder::new()
             .with_title("Mandelbrot Set Visualizer")
@@ -35,11 +36,11 @@ fn main() -> Result<(), Error> {
     let mut pixels = {
         let window_size = window.inner_size();
         let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-        Pixels::new(WIDTH, HEIGHT, surface_texture)?
+        Pixels::new(VIEWPORT_WIDTH, VIEWPORT_HEIGHT, surface_texture)?
     };
 
     let g = colorgrad::rainbow();
-    let mandelbrot = Mandelbrot::new(WIDTH, HEIGHT, DIVERGE_ITERATIONS, DIVERGE_THRESHOLD);
+    let mandelbrot = Mandelbrot::new(DIVERGE_ITERATIONS, DIVERGE_THRESHOLD);
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
@@ -50,10 +51,13 @@ fn main() -> Result<(), Error> {
                 let frame = pixels.frame_mut();
 
                 for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
-                    let x = (i % WIDTH as usize) as u32;
-                    let y = (i / WIDTH as usize) as u32;
+                    let x = (i % VIEWPORT_WIDTH as usize) as u32;
+                    let y = (i / VIEWPORT_WIDTH as usize) as u32;
 
-                    let color = if let Some(num) = mandelbrot.calculage_diverge_number(x, y) {
+                    let a = (x as f64 / VIEWPORT_WIDTH as f64) * 2.0 - 1.5;
+                    let b = (y as f64 / VIEWPORT_HEIGHT as f64) * 2.0 - 1.0;
+
+                    let color = if let Some(num) = mandelbrot.calculage_diverge_number(a, b) {
                         let brightness = num as f64 / DIVERGE_ITERATIONS as f64;
                         g.at(brightness).to_rgba8()
                     } else {
